53a1fcea7ecf6ccc1b53f7445e2b51efeaf3f0ec
Jo Eungyeong <joeungyeong23@gmail.com>
2024-11-25 01:04:12 +0900

    회사 검색 기능 추가

--- app/src/main/java/jo/toybreeze/CompanyAdaptor.java
+++ app/src/main/java/jo/toybreeze/CompanyAdaptor.java
@@ -1,25 +1,16 @@
 package jo.toybreeze;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.tabs.TabLayout;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,10 +18,12 @@ import jo.toybreeze.domain.Company;
 
 public class CompanyAdaptor extends RecyclerView.Adapter<CompanyAdaptor.ViewHolder> {
     private List<Company> companies;
+    private List<Company> allCompanies;
     private Bitmap bitmap;
 
     public CompanyAdaptor(List<Company> companies) {
-        this.companies = new ArrayList<>(companies);
+        this.companies = companies;
+        this.allCompanies = new ArrayList<>(companies);
     }
 
     @NonNull
@@ -69,5 +62,20 @@ public class CompanyAdaptor extends RecyclerView.Adapter<CompanyAdaptor.ViewHold
     @Override
     public int getItemCount() {
         return companies.size();
+    }
+
+    public void filter(String text) {
+        companies.clear();
+        if (text.isEmpty()) {
+            companies.addAll(allCompanies);
+        } else {
+            text = text.toLowerCase();
+            for (Company company : allCompanies) {
+                if (company.getName().toLowerCase().contains(text)) {
+                    companies.add(company);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/jo/toybreeze/FragmentSubscribe.java
+++ app/src/main/java/jo/toybreeze/FragmentSubscribe.java
@@ -1,10 +1,10 @@
 package jo.toybreeze;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,13 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,14 +23,17 @@ import jo.toybreeze.domain.Image;
 
 public class FragmentSubscribe extends Fragment {
     private static final String TAG = FragmentSubscribe.class.getSimpleName();
+    private SearchView searchView;
     private RecyclerView recyclerView;
     private FirebaseFirestore db;
+    private CompanyAdaptor companyAdaptor;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_subscribe, container, false);
 
+        searchView = view.findViewById(R.id.searchView);
         recyclerView = view.findViewById(R.id.recyclerView);
         db = FirebaseFirestore.getInstance();
 
@@ -50,9 +48,22 @@ public class FragmentSubscribe extends Fragment {
 
                     LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
                     recyclerView.setLayoutManager(linearLayoutManager);
-                    CompanyAdaptor companyAdaptor = new CompanyAdaptor(companies);
+                    companyAdaptor = new CompanyAdaptor(companies);
                     recyclerView.setAdapter(companyAdaptor);
                 });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                companyAdaptor.filter(newText);
+                return false;
+            }
+        });
         return view;
     }
 }
